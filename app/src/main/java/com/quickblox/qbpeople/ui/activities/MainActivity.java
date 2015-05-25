b9c7f38c53abab8abc966615d18072ab270069b7
tatanka987 <tatanka.tvv@gmail.com>
2015-05-26 01:27:09 +0300

    - added drawer navigator with items and icons;

--- app/src/main/java/com/quickblox/qbpeople/ui/activities/MainActivity.java
+++ app/src/main/java/com/quickblox/qbpeople/ui/activities/MainActivity.java
@@ -1,21 +1,37 @@
 package com.quickblox.qbpeople.ui.activities;
 
-import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.quickblox.qbpeople.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private Drawer drawerResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolBar();
+        createDrawer(toolbar);
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,5 +53,64 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerResult != null && drawerResult.isDrawerOpen()){
+            drawerResult.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void initToolBar(){
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        if (toolbar != null){
+            setSupportActionBar(toolbar);
+        }
+    }
+
+    private void createDrawer(Toolbar toolbar) {
+
+        AccountHeader accountHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.quick_blox_transparent)
+                .withHeaderBackgroundScaleType(ImageView.ScaleType.FIT_CENTER)
+                .build();
+
+        drawerResult = new DrawerBuilder(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggleAnimated(true)
+                .withAnimateDrawerItems(true)
+                .withDisplayBelowToolbar(true)
+                .withAccountHeader(accountHeader)
+                .addDrawerItems(
+                        new PrimaryDrawerItem()
+                                .withName(R.string.update)
+                                .withIcon(R.drawable.ic_sync_black_18dp)
+                                .withIdentifier(1),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem()
+                                .withName(R.string.go_to_website)
+                                .withIcon(R.drawable.ic_web_black_18dp)
+                                .withIdentifier(2),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem()
+                                .withName(R.string.in_social_networks)
+                                .withIdentifier(3),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.social_facebook)
+                                .withIcon(R.drawable.ic_facebook_box_grey600_18dp),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.social_vk)
+                                .withIcon(R.drawable.ic_vk_grey600_18dp),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.social_twitter)
+                                .withIcon(R.drawable.ic_twitter_grey600_18dp),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.social_youtube)
+                                .withIcon(R.drawable.ic_youtube_play_grey600_18dp))
+                .build();
     }
 }
